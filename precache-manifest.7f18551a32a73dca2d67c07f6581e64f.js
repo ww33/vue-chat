69f960401b9b861f002d09715b302cf95a4f7c34
ww33 <yinfo333@gmail.com>
2020-06-11 08:41:09 +0300

    Updates

---
+++ precache-manifest.7f18551a32a73dca2d67c07f6581e64f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c5b4e923ad73c384f3b191e256c411",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa2dea1bead6128654b1",
+    "url": "/static/css/main.c4ed4996.chunk.css"
+  },
+  {
+    "revision": "16ce468feac089e2b1c4",
+    "url": "/static/js/2.9b50ebec.chunk.js"
+  },
+  {
+    "revision": "dee959b718ac48efcb3dce24b9ae24d5",
+    "url": "/static/js/2.9b50ebec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa2dea1bead6128654b1",
+    "url": "/static/js/main.6efa33f0.chunk.js"
+  },
+  {
+    "revision": "b3c9d75a216011868627",
+    "url": "/static/js/runtime-main.5080b4b8.js"
+  },
+  {
+    "revision": "bc9387e41282bda7d5bd12bbc09fa02a",
+    "url": "/static/media/zoom-in.bc9387e4.svg"
+  },
+  {
+    "revision": "72523ba9c9b21e3c139148d52b8868ef",
+    "url": "/static/media/zoom-out.72523ba9.svg"
+  }
+]);
